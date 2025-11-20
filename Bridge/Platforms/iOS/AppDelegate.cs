05bebff8f37c2ae6bd376262fc0f855f1f00d32c
kukudia <138330931+kukudia@users.noreply.github.com>
2025-11-21 01:45:35 +0800

    Create .NET MAUI project

---
+++ Bridge/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Bridge
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
